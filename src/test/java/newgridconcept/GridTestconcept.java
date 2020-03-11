fa2cecf153cd3513e216e31f300721a3559e1ca2
MOHAMMAD ROBIUL <md.rislam1980@gmail.com>
2020-03-11 11:18:26 -0400

    second round

--- src/test/java/newgridconcept/GridTestconcept.java
+++ src/test/java/newgridconcept/GridTestconcept.java
@@ -8,18 +8,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-public class NewSeleniumGridconcept {
-
-	public static void main(String[] args) throws MalformedURLException {
+public class GridTestconcept {
+	public static void main(String[] args) throws MalformedURLException  {
 		DesiredCapabilities cap=DesiredCapabilities.chrome();
 		cap.setPlatform(Platform.WIN10);
 		URL url=new URL (" http://192.168.81.1:39061/wd/hub");
 		WebDriver driver=new RemoteWebDriver(url, cap);
-		driver.get("https://www.google.com");
+		driver.get("https://www.facebook.com");
 		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
 		driver.close();
 		driver.quit();
-
 	}
+//		DesiredCapabilities cap=DesiredCapabilities.chrome();
+//		cap.setPlatform(Platform.WIN10);
+//		URL url=new URL (" http://192.168.81.1:39061/wd/hub");
+//		WebDriver driver=new RemoteWebDriver(url, cap);
+//		driver.get("https://www.google.com");
+//		System.out.println(driver.getTitle());
+//		driver.close();
+//		driver.quit();	}
 
 }
